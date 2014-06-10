6e146d3be0870fc1fe1a83ae3ebbc5754b09aa0a
PlenipotentSS <stevensonhoyt@icloud.com>
2014-06-10 12:06:51 -0700

    initial commit for new UI

---
+++ Swing Local/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Swing Local
+//
+//  Created by Steven Stevenson on 6/10/14.
+//  Copyright (c) 2014 Steven Stevenson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Swing Local/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Swing Local
+//
+//  Created by Steven Stevenson on 6/10/14.
+//  Copyright (c) 2014 Steven Stevenson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
